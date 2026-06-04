e432962ad91fb9e822b8bcc270eb57d8fa668df8
Aura Lily Vulcano <millenomi@me.com>
2026-06-04 16:18:52 -0700

    Ensure impl.c can build etc., 2

--- Sources/AndroidBackendShim/impl.c
+++ Sources/AndroidBackendShim/impl.c
@@ -1,5 +1,7 @@
 #include "header.h"
 
+#if SCUI_HAS_ANDROID_LOG_H
 void android_log(int priority, const char *namespace, const char *message) {
     __android_log_write(priority, namespace, message);
 }
+#endif
